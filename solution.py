faa4b568846fb20c89429188d545395f6f9ee713
saeveritt <sae8958@nyu.edu>
2022-09-19 09:58:30 -0500

    remove print

--- solution.py
+++ solution.py
@@ -4,12 +4,10 @@ import sys
 def run(port: int =13331):
 
   host: str = "127.0.0.1"
-  print(f'HOST: {host}')
   server_socket: socket.SocketType = socket.socket()
   server_socket.bind( (host, port) )
 
   while True:
-    print('Ready to serve...')
     server_socket.listen(1)
     conn, address = server_socket.accept()
 
@@ -21,7 +19,6 @@ def run(port: int =13331):
         response = b'HTTP/1.1 200 OK\r\n'
         response += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
         response += f.read()
-        print(response)
         conn.send(response.decode().encode("utf8"))
         conn.close()
     
