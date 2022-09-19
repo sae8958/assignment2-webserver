6310d45a6a464a7814e549bdeffa544ccdf178a8
saeveritt <sae8958@nyu.edu>
2022-09-19 09:51:38 -0500

    update

--- solution.py
+++ solution.py
@@ -3,28 +3,39 @@ import sys
 
 def run(port: int =13331):
 
-  host: str = socket.gethostname()
-
+  host: str = "127.0.0.1"
+  print(f'HOST: {host}')
   server_socket: socket.SocketType = socket.socket()
   server_socket.bind( (host, port) )
-  server_socket.listen(2)
-  
-  conn, address = server_socket.accept()
 
   while True:
     print('Ready to serve...')
+    server_socket.listen(1)
+    conn, address = server_socket.accept()
 
     try:
 
       message = conn.recv(1024).decode()
-      print(f'Message: {message}\n Address: {address}')
+      filename = f'.{message.split()[1]}'
+      with open(filename,"rb") as f:
+        response = b'HTTP/1.1 200 OK\r\n'
+        response += b"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+        response += f.read()
+        print(response)
+        conn.send(response)
+        conn.close()
     
+    except FileNotFoundError:
+      response = b'HTTP/1.1 404 File Not Found\r\n'
+      conn.send(response)
+      conn.close()
+
     except Exception as e:
-      print(e)
       break
 
   server_socket.close()
-  sys.exit()  # Terminate the program after sending the corresponding data
+  sys.exit()
+
 
 if __name__ == "__main__":
   run( port=13331 )
