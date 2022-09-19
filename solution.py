f1e62e993f7357480e572a8fd09b46ca2691f86a
sae8958 <sae8958@nyu.edu>
2022-09-19 09:04:37 -0500

    update

--- solution.py
+++ solution.py
@@ -1,62 +1,30 @@
-# import socket module
-from socket import *
-# In order to terminate the program
+import socket
 import sys
 
-def webServer(port=13331):
-  serverSocket = socket(AF_INET, SOCK_STREAM)
-  
-  #Prepare a server socket
-  serverSocket.bind(("", port))
-  
-  #Fill in start
+def run(port: int =13331):
 
-  #Fill in end
+  host: str = socket.gethostname()
+
+  server_socket: socket.SocketType = socket.socket()
+  server_socket.bind( (host, port) )
+  server_socket.listen(2)
+  
+  conn, address = server_socket.accept()
 
   while True:
-    #Establish the connection
-    
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
-    
+
     try:
-      message = #Fill in start -a client is sending you a message   #Fill in end 
-      filename = message.split()[1]
-      
-      #opens the client requested file. 
-      #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], #fill in start #fill in end)
-      #fill in end
-      
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"\
-      #Fill in start -This variable can store your headers you want to send for any valid or invalid request. 
-      #Content-Type above is an example on how to send a header as bytes
-      #Fill in end
 
-      #Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok? 
-      #Fill in start
-
-      #Fill in end
-
-      #Send the content of the requested file to the client
-      for i in f: #for line in file
-        #Fill in start - send your html file contents #Fill in end 
-      connectionSocket.close() #closing the connection socket
-      
+      message = conn.recv(1024).decode()
+      print(f'Message: {message}\n Address: {address}')
+    
     except Exception as e:
-      # Send response message for invalid request due to the file not being found (404)
-      #Fill in start
+      print(e)
+      break
 
-      #Fill in end
-
-
-      #Close client socket
-      #Fill in start
-
-      #Fill in end
-
-  serverSocket.close()
+  server_socket.close()
   sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-  webServer(13331)
+  run( port=13331 )
